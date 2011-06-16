4ec3eeb92dbd326e44908d3670952f14ed4b07d0
Greg Moser <greg@gregmoser.com>
2011-06-15 19:35:02 -0700

    Fixed some style and javascript stuff.

--- js/documentation.js
+++ js/documentation.js
@@ -4,19 +4,33 @@ jQuery(document).ready(function () {
 });
 
 function bindFunctions() {
-	jQuery('img.navexpander').click(function() {
-		jQuery(this).removeClass('navexpander');
+	jQuery('.navexpander').click(function() {
 		jQuery(this).addClass('navcontractor');
-		jQuery(this).attr('src', minusImage);
+		jQuery(this).siblings('.navexpander').addClass('navcontractor');
+		jQuery(this).removeClass('navexpander');
+		jQuery(this).siblings('.navexpander').removeClass('navexpander');
+		if(jQuery(this).is('img')) {
+			jQuery(this).attr('src', minusImage);	
+		} else {
+			jQuery(this).siblings('img').attr('src', minusImage);	
+		}
 		jQuery(this).siblings('ul').show();
+		
 		bindFunctions();
 	});
 	
-	jQuery('img.navcontractor').click(function() {
-		jQuery(this).removeClass('navcontractor');
+	jQuery('.navcontractor').click(function() {
 		jQuery(this).addClass('navexpander');
-		jQuery(this).attr('src', plusImage);
+		jQuery(this).siblings('.navcontractor').addClass('navexpander');
+		jQuery(this).removeClass('navcontractor');
+		jQuery(this).siblings('.navcontractor').removeClass('navcontractor');
+		if(jQuery(this).is('img')) {
+			jQuery(this).attr('src', plusImage);	
+		} else {
+			jQuery(this).siblings('img').attr('src', plusImage);	
+		}
 		jQuery(this).siblings('ul').hide();
+		
 		bindFunctions();
 	});
 }
